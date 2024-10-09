a68eee095b9f95b70768910f11b92c4cb47c9bed
Aniket Inamdar <asinamdar2024@gmail.com>
2024-10-09 23:21:52 +0530

    Added game loop

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,12 +1,44 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS = 120;
 
     public Game() {
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+    }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS;
+        int frames = 0;
+        long now;
+        long lastChecked = System.currentTimeMillis();
+        long lastFrame = System.nanoTime();
+
+        while(true) {
+            now = System.nanoTime();
+            if (now - lastFrame >= timePerFrame) {
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+            if (System.currentTimeMillis() - lastChecked >= 1000) {
+                lastChecked = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,13 +4,19 @@ import inputs.KeyboardInput;
 import inputs.MouseInput;
 
 import javax.swing.JPanel;
-import java.awt.Graphics;
+import java.awt.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
     private MouseInput mouseInput;
-    private int dx = 300, dy = 200;
+
+    private float dx = 300.0f, dy = 200.0f;
+    private float xDir = 1.0f, yDir = 1.0f;
+    private Color color = new Color(100, 100, 100);
+    private Random random = new Random();
 
     public GamePanel() {
+        random = new Random();
         mouseInput = new MouseInput(this);
         addKeyListener(new KeyboardInput(this));
         addMouseListener(mouseInput);
@@ -19,22 +25,43 @@ public class GamePanel extends JPanel {
 
     public void changeDx(int val) {
         this.dx += val;
-        repaint();
     }
 
     public void changeDy(int val) {
         this.dy += val;
-        repaint();
     }
 
     public void setPosition(int x, int y) {
         this.dx = x;
         this.dy = y;
-        repaint();
+    }
+
+    private Color randomColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return new Color(r, g, b);
+    }
+
+    private void updateRectangle() {
+        dx += xDir;
+        if (dx > 800 || dx < 0) {
+            xDir *= -1;
+            color = randomColor();
+        }
+
+        dy += yDir;
+        if (dy > 600 || dy < 0) {
+            yDir *= -1;
+            color = randomColor();
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(dx, dy, 200, 50);
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int) dx, (int) dy, 200, 50);
     }
 }
