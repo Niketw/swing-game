eba9101ae2c9390c9ae15eb27109dab3cc47506e
Aniket Inamdar <asinamdar2024@gmail.com>
2024-10-09 15:39:25 +0530

    Initial commit

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,2 +1,16 @@
-package main;public class GamePanel {
+package main;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+public class GamePanel extends JPanel {
+
+    public GamePanel() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.fillRect(300, 200, 200, 50);
+    }
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,2 +1,14 @@
-package main;public class GameWindow {
+package main;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame {
+
+    public GameWindow(GamePanel gamePanel) {
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(gamePanel);
+        setVisible(true);
+    }
 }
